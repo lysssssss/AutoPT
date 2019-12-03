4193cde48b2d971e000828e778588a84998b69d5
Eason <jimmy591986780@qq.com>
2019-12-03 23:06:24 +0800

    Rewrite read configuration file

--- Myconfig.py
+++ Myconfig.py
@@ -7,59 +7,117 @@ from jsmin import jsmin
 class Config(object):
     """read config"""
 
+    def __getitem__(self, key):
+        if key.upper() == 'BYR':
+            return self.byrconfig
+        elif key.upper() == 'TJU':
+            return self.byrconfig
+        return {}
+
     def __init__(self):
+        self.byrconfig = {
+            'switch': False,
+            'checktrackerhttps': False,
+            'checkptmode': 1,
+            'maincategory': '',
+            'subcategory': [],
+            'qbaddr': '',
+            'capacity': 0,
+            'capacityuint': 'GB',
+            'capacitynum': 0,
+            'dlroot': '',
+            'auto_flag': False,
+            'intervaltime': 60,
+            'keeptorrenttime': 0
+        }
+        self.tjuconfig = {
+            'switch': False,
+            'checktrackerhttps': False,
+            'checkptmode': 1,
+            'maincategory': '',
+            'subcategory': [],
+            'qbaddr': '',
+            'capacity': 0,
+            'capacityuint': 'GB',
+            'capacitynum': 0,
+            'dlroot': '',
+            'auto_flag': False,
+            'intervaltime': 60,
+            'keeptorrenttime': 0
+        }
         if os.path.exists('config.json'):
             f = open('config.json', 'r', encoding='utf-8')
             text = jsmin(f.read())
             # text = f.read()
             f.close()
             paras = json.loads(text)
-            if 'torrent_download_root' in paras:
-                self._dlroot = paras['torrent_download_root']
-                if not self._dlroot.endswith('/'):
-                    self._dlroot = self._dlroot + '/'
-            else:
-                self._dlroot = ''
-            if 'Auto_management' in paras:
-                self._auto_flag = paras['Auto_management']
-            else:
-                self._auto_flag = False
-            if 'test' in paras:
-                self.test = paras['test']
-            else:
-                self.test = ''
-            if 'IntervalTime' in paras:
-                self._intervaltime = paras['IntervalTime']
-            else:
-                self._intervaltime = 60
-            if 'username' in paras:
-                self._username = paras['username']
-            else:
-                self._username = ''
-            if 'password' in paras:
-                self._password = paras['password']
-            else:
-                self._password = ''
-            if 'CapacityNum' in paras:
-                self._capacitynum = paras['CapacityNum'] if paras['CapacityNum'] > -1 else 10485760
-            else:
-                self._capacitynum = 0
-            if 'QB_WebAddr' in paras:
-                self._qbaddr = paras['QB_WebAddr']
-            else:
-                self._qbaddr = ''
-            if 'CapacityUint' in paras:
-                self._capacityuint = paras['CapacityUint'].upper() if paras['CapacityUint'].upper() in ['GB',
-                                                                                                        'TB'] else 'GB'
-            else:
-                self._capacityuint = 'GB'
-            if 'MainCategory' in paras:
-                self._maincategory = paras['MainCategory'][0] if len(paras['MainCategory'][:1]) > 0 else ''
-                self._subcategory = paras['MainCategory'][1:]
-                self._subcategory = list(set(self._subcategory))
-            else:
-                self._maincategory = ''
-                self._subcategory = []
+            self.readlogconfig(paras)
+            self.readbyrconfig(paras)
+            self.readtjuconfig(paras)
+        else:
+            self._logsavetime = 7
+            self._loglevel = 'info'
+
+    def transcapacity(self, config):
+        if config['capacityuint'] == 'GB':
+            config['capacity'] = config['capacitynum']
+        elif config['capacityuint'] == 'TB':
+            config['capacity'] = config['capacitynum'] * 1024
+        elif config['capacityuint'] == 'MB':
+            config['capacity'] = config['capacitynum'] / 1024
+        else:
+            config['capacity'] = 0
+
+    def readbyrconfig(self, param):
+        if 'BYR' in param:
+            paras = param['BYR']
+            self.readcommonconfig(paras, self.byrconfig)
+            # To add custom config here
+
+    def readtjuconfig(self, param):
+        if 'TJU' in param:
+            paras = param['TJU']
+            self.readcommonconfig(paras, self.tjuconfig)
+            # To add custom config here
+
+    def readcommonconfig(self, paras, pt_config):
+        if 'switch' in paras:
+            pt_config['switch'] = paras['switch']
+        if 'torrent_download_root' in paras:
+            pt_config['dlroot'] = paras['torrent_download_root']
+            if not pt_config['dlroot'].endswith('/'):
+                pt_config['dlroot'] = pt_config['dlroot'] + '/'
+        if 'Auto_management' in paras:
+            pt_config['auto_flag'] = paras['Auto_management']
+        if 'IntervalTime' in paras:
+            pt_config['intervaltime'] = paras['IntervalTime']
+        if 'CapacityNum' in paras:
+            pt_config['capacitynum'] = paras['CapacityNum'] if paras['CapacityNum'] > -1 else 10485760
+        if 'QB_WebAddr' in paras:
+            pt_config['qbaddr'] = paras['QB_WebAddr']
+        if 'CapacityUint' in paras:
+            pt_config['capacityuint'] = paras['CapacityUint'].upper() \
+                if paras['CapacityUint'].upper() in ['GB', 'TB'] else 'GB'
+        if 'MainCategory' in paras:
+            pt_config['maincategory'] = paras['MainCategory'][0] if len(paras['MainCategory'][:1]) > 0 else ''
+            pt_config['subcategory'] = paras['MainCategory'][1:]
+            pt_config['subcategory'] = list(set(pt_config['subcategory']))
+        if 'CheckPTMode' in paras:
+            pt_config['checkptmode'] = paras['CheckPTMode'] if paras['CheckPTMode'] in [1, 2] else 1
+        if 'KeepTorrentTime' in paras:
+            pt_config['keeptorrenttime'] = paras['KeepTorrentTime'] if paras['KeepTorrentTime'] >= 0 else 0
+
+        if 'CheckTrackerHTTPS' in paras:
+            pt_config['checktrackerhttps'] = paras['CheckTrackerHTTPS']
+        # 转换磁盘容量
+        self.transcapacity(pt_config)
+        # 处理异常情况，强制关闭磁盘管理
+        if pt_config['capacitynum'] == 0:
+            pt_config['auto_flag'] = False
+
+    def readlogconfig(self, para):
+        if 'log' in para:
+            paras = para['log']
             if 'LogLevel' in paras:
                 self._loglevel = paras['LogLevel'].lower()
                 self._loglevel = self._loglevel if self._loglevel in ['info', 'debug'] else 'info'
@@ -69,63 +127,45 @@ class Config(object):
                 self._logsavetime = paras['LogSaveTime']
             else:
                 self._logsavetime = 7
-            if 'CheckPTMode' in paras:
-                self._checkptmode = paras['CheckPTMode'] if paras['CheckPTMode'] in [1, 2] else 1
-            else:
-                self._checkptmode = 1
-            if 'KeepTorrentTime' in paras:
-                self._keeptorrenttime = paras['KeepTorrentTime'] if paras['KeepTorrentTime'] >= 0 else 0
-            else:
-                self._keeptorrenttime = 0
-            if 'CheckTrackerHTTPS' in paras:
-                self._checktrackerhttps = paras['CheckTrackerHTTPS']
-            else:
-                self._checktrackerhttps = False
-        else:
-            self._checktrackerhttps = False
-            self._checkptmode = 1
-            self._logsavetime = 7
-            self._loglevel = 'info'
-            self._maincategory = ''
-            self._subcategory = []
-            self._qbaddr = ''
-            self._capacity = 0
-            self._capacityuint = 'GB'
-            self._capacitynum = 0
-            self._dlroot = ''
-            self._auto_flag = False
-            self.test = ''
-            self._intervaltime = 60
-            self._username = ''
-            self._password = ''
-            self._keeptorrenttime = 0
-        # 转换磁盘容量
-        self.transcapacity()
-        # 处理异常情况，强制关闭磁盘管理
-        if self._capacitynum == 0:
-            self._auto_flag = False
 
-    def transcapacity(self):
-        if self._capacityuint == 'GB':
-            self._capacity = self._capacitynum
-        elif self._capacityuint == 'TB':
-            self._capacity = self._capacitynum * 1024
-        elif self._capacityuint == 'MB':
-            self._capacity = self._capacitynum / 1024
-        else:
-            self._capacity = 0
+    def getnameconfig(self):
+        return {
+            'BYR': self.byrconfig,
+            'TJU': self.tjuconfig
+            }
 
-    @property
-    def checktrackerhttps(self):
-        return self._checktrackerhttps
+    def switch(self, name):
+        return self.getnameconfig()[name.upper()]['switch']
 
-    @property
-    def keeptorrenttime(self):
-        return self._keeptorrenttime
+    def checktrackerhttps(self, name):
+        return self.getnameconfig()[name.upper()]['checktrackerhttps']
 
-    @property
-    def checkptmode(self):
-        return self._checkptmode
+    def keeptorrenttime(self, name):
+        return self.getnameconfig()[name.upper()]['keeptorrenttime']
+
+    def checkptmode(self, name):
+        return self.getnameconfig()[name.upper()]['checkptmode']
+
+    def maincategory(self, name):
+        return self.getnameconfig()[name.upper()]['maincategory']
+
+    def subcategory(self, name):
+        return self.getnameconfig()[name.upper()]['subcategory']
+
+    def qbaddr(self, name):
+        return self.getnameconfig()[name.upper()]['qbaddr']
+
+    def capacity(self, name):
+        return self.getnameconfig()[name.upper()]['capacity']
+
+    def dlroot(self, name):
+        return self.getnameconfig()[name.upper()]['dlroot']
+
+    def autoflag(self, name):
+        return self.getnameconfig()[name.upper()]['autoflag']
+
+    def intervaltime(self, name):
+        return self.getnameconfig()[name.upper()]['intervaltime']
 
     @property
     def loglevel(self):
@@ -135,42 +175,8 @@ class Config(object):
     def logsavetime(self):
         return self._logsavetime
 
-    @property
-    def maincategory(self):
-        return self._maincategory
-
-    @property
-    def subcategory(self):
-        return self._subcategory
-
-    @property
-    def qbaddr(self):
-        return self._qbaddr
-
-    @property
-    def capacity(self):
-        return self._capacity
-
-    @property
-    def dlroot(self):
-        return self._dlroot
-
-    @property
-    def autoflag(self):
-        return self._auto_flag
-
-    @property
-    def intervaltime(self):
-        return self._intervaltime
-
-    @property
-    def username(self):
-        return self._username
-
-    @property
-    def password(self):
-        return self._password
-
 
 if __name__ == '__main__':
-    print(Config())
+    config = Config()
+    print (config['BYR'])
+    pass
