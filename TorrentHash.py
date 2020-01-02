83b5b733a83df3ad3b4715ec0cd1d3e4e81f1a01
Eason <jimmy591986780@qq.com>
2020-01-02 23:01:07 +0800

    TorrentHash:Use libtorrent to calculate torrent hash

--- TorrentHash.py
+++ TorrentHash.py
@@ -1,33 +1,25 @@
-import base64
 import traceback
-import magneturi
-
+import libtorrent
 import globalvar as gl
 
 
 def get_torrent_hash40(data):
     try:
-        mangetlink = magneturi.from_torrent_data(data)
-        mangetlink = mangetlink[mangetlink.find('btih') + 5:mangetlink.find('btih') + 5 + 32]
-        b16Hash = base64.b16encode(base64.b32decode(mangetlink))
-        b16Hash = b16Hash.lower()
-        b16Hash = str(b16Hash, "utf-8")
-        return b16Hash
+        with open('ttmp.dat', 'wb') as f:
+            f.write(data)
+        return str(libtorrent.torrent_info('ttmp.dat').info_hash())
+        # mangetlink = magneturi.from_torrent_data(data)
+        # mangetlink = mangetlink[mangetlink.find('btih') + 5:mangetlink.find('btih') + 5 + 32]
+        # b16Hash = base64.b16encode(base64.b32decode(mangetlink))
+        # b16Hash = b16Hash.lower()
+        # b16Hash = str(b16Hash, "utf-8")
+        # return b16Hash
     except BaseException as e:
         gl.get_value('logger').logger.exception(traceback.format_exc())
 
 
-# if __name__ == '__main__':
-#     torrent = open('123.torrent', 'rb').read()
-#     print(get_torrent_hash40(torrent))
-#     # 计算meta数据
-#     metadata = bencode.bdecode(torrent)
-#     hash_content = bencode.bencode(metadata[b'info'])
-#     digest = hashlib.sha1(hash_content).digest()
-#     b32hash = base64.b32encode(digest)
-#     print(b32hash.decode())
-#     b16Hash = base64.b16encode(base64.b32decode(b32hash.decode()))
-#     b16Hash = b16Hash.lower()
-#     b16Hash = str(b16Hash, "utf-8")
-#     print(b16Hash)
+if __name__ == '__main__':
 
+    with open('123.torrent', 'rb') as f:
+        aaa = f.read()
+        print(get_torrent_hash40(aaa))
